a6ef3eda00edd836ad81ad2e2db72743ccb00781
chris carneiro <christ.carneiro@gmail.com>
2015-01-15 13:35:41 +0100

    View injection library "butterknife" usage added to the main activity

--- app/src/main/java/net/opencurlybraces/android/projects/androidpractices/MainActivity.java
+++ app/src/main/java/net/opencurlybraces/android/projects/androidpractices/MainActivity.java
@@ -5,14 +5,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.hello_text) TextView mHelloText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 
 
