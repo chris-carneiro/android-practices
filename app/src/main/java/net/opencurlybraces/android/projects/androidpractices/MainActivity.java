9beffaa9909ea299d069beabc6fad64fdac30644
chris carneiro <christ.carneiro@gmail.com>
2015-01-15 14:17:59 +0100

    Trim Memory constants case added in the callback OnTrimMemory(int level). Each case is documented

--- app/src/main/java/net/opencurlybraces/android/projects/androidpractices/MainActivity.java
+++ app/src/main/java/net/opencurlybraces/android/projects/androidpractices/MainActivity.java
@@ -14,7 +14,7 @@ import butterknife.InjectView;
 public class MainActivity extends ActionBarActivity {
 
 
-    @InjectView(R.id.hello_text)
+    @InjectView (R.id.hello_text)
     TextView mHelloText;
 
     @Override
@@ -22,7 +22,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /**
-         * Apply butterknife views injection when annotated, no need to call any findViewById() or so..
+         * Apply butterknife views injection when annotated, no need to call any findViewById()
+         * or so..
          */
         ButterKnife.inject(this);
     }
@@ -54,8 +55,56 @@ public class MainActivity extends ActionBarActivity {
     public void onTrimMemory(int level) {
         super.onTrimMemory(level);
 
-        if (level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
-            //release UI Resources here
+
+        switch (level) {
+            case TRIM_MEMORY_UI_HIDDEN:
+                //release UI Resources here
+
+            case TRIM_MEMORY_RUNNING_MODERATE:
+                /**
+                 *  app is running and not considered killable, but the device is running
+                 *  low on memory and the system is actively killing processes in the LRU cache.
+                 */
+            case TRIM_MEMORY_RUNNING_LOW:
+                /**
+                 * Your app is running and not considered killable, but the device is running much
+                 * lower on memory so you should release unused resources to improve system
+                 * performance (which directly impacts your app's performance). */
+            case TRIM_MEMORY_RUNNING_CRITICAL:
+                /**
+                 * Your app is still running, but the system has already killed most of the
+                 * processes in the LRU cache, so you should release all non-critical resources
+                 * now. If the system cannot reclaim sufficient amounts of RAM,
+                 * it will clear all of the LRU cache and begin killing processes that the system
+                 * prefers to keep alive, such as those hosting a running service.
+                 */
+
+            case TRIM_MEMORY_BACKGROUND:
+                /**
+                 * The system is running low on memory and your process is near the beginning of
+                 * the LRU list. Although your app process is not at a high risk of being killed,
+                 * the system may already be killing processes in the LRU cache. You should
+                 * release resources that are easy to recover so your process will remain in the
+                 * list and resume quickly when the user returns to your app.
+                 */
+
+            case TRIM_MEMORY_MODERATE:
+                /**
+                 * The system is running low on memory and your process is near the middle of the
+                 * LRU list. If the system becomes further constrained for memory,
+                 * there's a chance your process will be killed.
+                 */
+
+            case TRIM_MEMORY_COMPLETE:
+                /**
+                 * The system is running low on memory and your process is one of the first to be
+                 * killed if the system does not recover memory now. You should release
+                 * everything that's not critical to resuming your app state.
+                 */
+            default:
+                //do something
+
         }
     }
+
 }
