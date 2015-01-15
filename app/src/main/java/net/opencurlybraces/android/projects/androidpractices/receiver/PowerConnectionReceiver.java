a9d7e18cbacf9cd750736fd771a14a6e7353fa4d
chris carneiro <christ.carneiro@gmail.com>
2015-01-16 00:13:51 +0100

    Battery status broadcast receiver class and package created. TODO note: define receiver in manifest

---
+++ app/src/main/java/net/opencurlybraces/android/projects/androidpractices/receiver/PowerConnectionReceiver.java
@@ -0,0 +1,18 @@
+package net.opencurlybraces.android.projects.androidpractices.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receiver thats listen to changes in power status changes
+ * <p/>
+ * TODO implement and define in manifest
+ * Created by chris on 16/01/15.
+ */
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
